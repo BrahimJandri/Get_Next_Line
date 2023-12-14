d4b771d40becef5e7e81069bc645f797b38cb5e8
bjandri <bjandri@c4r9p6.1337.ma>
2023-12-14 12:17:49 +0100

    fixed some error

--- get_next_line.h
+++ get_next_line.h
@@ -1,17 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjandri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 09:08:51 by bjandri           #+#    #+#             */
+/*   Updated: 2023/12/13 09:08:52 by bjandri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 32
+#  define BUFFER_SIZE 10
 # endif
 
 char	*get_next_line(int fd);
 int		ft_strlen(char *str);
 char	*ft_strjoin(char *s1, char *s2);
-
-
+char	*ft_strchr(char *str, int c);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,53 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjandri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 09:09:14 by bjandri           #+#    #+#             */
+/*   Updated: 2023/12/13 09:09:15 by bjandri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-size_t    ft_strlen(const char *s)
+int	ft_strlen(char *str)
 {
-    size_t	i;
+	size_t	i;
 
-    i = 0;
-    while (s && s[i])
-        i++;
-    return (i);
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
 }
 
-char    *ft_strchr(char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
-    while (s && *s)
-    {
-        if (*s == (char)c)
-            return ((char *)s);
-        s++;
-    }
-    if (c == '\0')
-        return ((char *)s);
-    return (NULL);
+	while (s && *s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
 }
 
-char    *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    char	*str;
-    size_t	i;
-    size_t	j;
+	char	*str;
+	size_t	i;
+	size_t	j;
 
-    if(!s1)
-    {
-        s1 = malloc(sizeof(char));
-        s1[0] = '\0';
-    }
-    if (!s1 || !s2)
-        return (NULL);
-    str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-    if (!str)
-        return (NULL);
-    i = -1;
-    j = 0;
-    if(s1)
-        while (s1[++i])
-            str[i] = s1[i];
-    while (s2[j])
-        str[i++] = s2[j++];
-    str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
-    free(s1);
-    return (str);
+	if (!s1)
+	{
+		s1 = malloc(sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (s1)
+		while (s1[++i])
+			str[i] = s1[i];
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
 }
-
